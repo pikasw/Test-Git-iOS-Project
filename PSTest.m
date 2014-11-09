d6706c459b522d640e9bf51361d6e88bc75850f4
brianc <brianc@briancs-mac-mini.local>
2014-11-08 19:08:10 -0600

    added a test class

---
+++ PSTest.m
@@ -0,0 +1,13 @@
+//
+//  PSTest.m
+//  Git test
+//
+//  Created by brianc on 2014/11/08.
+//  Copyright (c) 2014 Pika Software Design. All rights reserved.
+//
+
+#import "PSTest.h"
+
+@implementation PSTest
+
+@end
